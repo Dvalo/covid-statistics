cc1ea6efeeaac730561d0041ab4141f18c36b99a
Dvalo <32298519+Dvalo@users.noreply.github.com>
2020-11-03 00:44:56 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import CountryCard from './components/CountryCard';
+import CountryList from './components/CountryList';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countriesData: [],
+      activeCountry: [],
+    };
+    this.setActiveCountry = this.setActiveCountry.bind(this);
+  }
+  componentDidMount() {
+    axios
+      .get("https://disease.sh/v3/covid-19/countries")
+      .then((response) => {
+        this.setState({ countriesData: response.data });
+        this.getRandomCountry();
+      })
+      .catch((error) => console.log(error));
+  }
+
+  getRandomCountry() {
+    let generateRandomInt = Math.floor(
+      Math.random() * this.state.countriesData.length
+    );
+    let getRandomCountry = this.state.countriesData[generateRandomInt];
+    this.setState({ activeCountry: getRandomCountry });
+  }
+
+  setActiveCountry(country) {
+    this.setState({ activeCountry: country });
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
+
+  render() {
+    return (
+      <div className="covid-tracker">
+        <h1 className="section-title">Country Card</h1>
+        <p className="section-instructions">
+          Random country has been displayed as an example, view instructions
+          below to display specific country of your choice.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <CountryCard currentCountry={this.state.activeCountry} />
+
+        <h1 className="section-title">Country List</h1>
+        <p className="section-instructions">
+          Click on the specific country name to view all the details about the
+          country.
+        </p>
+        <CountryList
+          countriesData={this.state.countriesData}
+          whenClicked={this.setActiveCountry}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CountryList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class CountryList extends React.Component {
+    render() {
+        if ( this.props.countriesData == null || this.props.countriesData.length === 0) {
+        return <div>Loading...</div>;
+        } else {
+        return (
+            <table id="countries_table">
+                <thead>
+                    <tr>
+                    <th>Country</th>
+                    <th>Total Cases</th>
+                    <th>Cases Today</th>
+                    <th>Deaths</th>
+                    <th>Deaths Today</th>
+                    <th>Recovered</th>
+                    <th>Recovered Today</th>
+                    <th>Active</th>
+                    <th>Critical</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.countriesData.map((elem) => (
+                    <tr key={elem.country} className="country_row">
+                        <th
+                        className="country-display"
+                        onClick={() => this.props.whenClicked(elem)}>
+                        <img
+                            src={elem.countryInfo.flag}
+                            alt={elem.country}
+                            className="country-img"></img>
+                        {elem.country}
+                        </th>
+                        <th>{elem.cases}</th>
+                        <th>{elem.todayCases}</th>
+                        <th>{elem.deaths}</th>
+                        <th>{elem.todayDeaths}</th>
+                        <th>{elem.recovered}</th>
+                        <th>{elem.todayRecovered}</th>
+                        <th>{elem.active}</th>
+                        <th>{elem.critical}</th>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+        }
+    }
+}
+
+export default CountryList;
